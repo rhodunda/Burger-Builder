c12e1904c5b18c9932c47bbd6184a154c4bf237d
Bryan Rhodunda <rhodunda@udel.edu>
2020-06-19 13:54:03 -0400

    added burger controls, burger, burger control

--- burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,48 @@
 import  React, { Component } from "react";
 import Aux from "../../hoc/Aux"
+import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/buildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    salad: 1,
+    cheese: 2,
+    meat: 3,
+    bacon: 2
+}
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0, 
+            meat: 0,
+        },
+        totalPrice: 2
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceAddition
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = (type) => {
+
+    }
     render() {
         return(
             <Aux>
-                <div>Burger</div>
-                <div>Build</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls 
+                ingredientAdded={this.addIngredientHandler}/>
             </Aux>
         )
     }
