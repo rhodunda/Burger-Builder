ad82d33793eb4042a3aaec779a743e4c4e95cb26
Bryan Rhodunda <rhodunda@udel.edu>
2020-06-19 14:30:01 -0400

    added pricing

--- burger_builder/src/components/Burger/buildControls/BuildControl/BuildControl.js
+++ burger_builder/src/components/Burger/buildControls/BuildControl/BuildControl.js
@@ -4,8 +4,17 @@ import classes from './BuildControl.css'
 const BuildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>less</button>
-        <button className={classes.More} onClick={props.added}>more</button>
+        <button 
+        className={classes.Less} 
+        onClick={props.removed} 
+        disabled={props.disabled}>
+        less
+        </button>
+        
+        <button className={classes.More}
+        onClick={props.added}>
+        more
+        </button>
     </div>
 )
 
